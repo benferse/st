50e658c15bd2c9411c5bb7c506a8232ecaf657a9
Ben Fersenheim <ben@bu.edu>
2022-01-02 20:40:53 -0500

    Update st default font with the right name from current Hasklug drops

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hasklug Nerd Font:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Hasklug Nerd Font Mono:Regular:size=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
