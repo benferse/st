17087028c4060b24fd58ec3310d67fdcdd9293cd
Ben Fersenheim <ben@bu.edu>
2022-04-15 14:47:56 -0400

    Switch to font family aliases

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JuliaMono Nerd Font Mono:Regular:size=14:antialias=true:autohint=true";
+static char *font = "monospace:Regular:size=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
